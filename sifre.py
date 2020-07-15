c132261d7207825522744326879c066d2f1008bb
Duvar <oyuntopu12gmail.com>
2020-07-15 22:20:16 +0300

    Update sifre.py

--- sifre.py
+++ sifre.py
@@ -1,17 +1,5 @@
-import time
 sifre = ["1","2","q","-","|","a",".","<","£","*","4","/",";","h","=","[","é","X","h","n","à","`","_","Ç","A","#","Q","Ö","9","5"]
 alfabe =["a","b","c","ç","d","e","f","g","ğ","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z"," "]
-
-
-
-
-
-
-
-
-
-
-
 
 while 1:
     if_i = 0
@@ -27,7 +15,7 @@ while 1:
                 if i == alfabe[if_i]:
                     print(sifre[if_i])
                     break
-            
+
     elif olay.lower() == "h":
         cozum = input("->")
         for i in cozum:
